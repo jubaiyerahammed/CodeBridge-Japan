f9d324f42d78da0ec53c3868129596c2994c876d
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-03-01 21:44:21 +0900

    divisible by 11

--- python1.py
+++ python1.py
@@ -13,3 +13,18 @@ else:
         
 
 
+rendom=int(input('enetr you number:'))
+
+remainder = rendom % 11
+if remainder==0:
+    print("The number is divisible by 9")
+    print("yes")
+    print('We did it')
+else:
+    print('The number is not divisible by 9 ')
+    print('Nooo')
+    print('We didnot it')
+
+        
+
+
