e2f7595eaddeed8012ffd6793d982783de372e24
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-03-02 13:03:26 +0900

    divisiblr number

---
+++ check_vowel.py
@@ -0,0 +1,30 @@
+"""
+ this program will take a character input and check if it a vowel or not.
+"""
+
+#a,e,i,o,u
+"""
+user=input('enter your charakter:')
+if user =='a':
+    print('the charakter is vowel')
+elif user =='e':
+    print('the charakter is vowel')
+elif user=='i':
+    print('the charakter is vowel')
+elif user =='o':
+    print('the charakter is vowel')
+elif user == 'u':
+    print('the charakter is vowel')
+else :
+    print('the charakter is a not vowel')                    
+"""
+
+user=input('enter your number:')
+#number divisible by 2,4,6
+if user % 2==0:
+    print('divisible by 2')
+if user % 4==o:
+    print('divisible by 4')
+if user % 5==0:
+    print('divisible by 5') 
+       
