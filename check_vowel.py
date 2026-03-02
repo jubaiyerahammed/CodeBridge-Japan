885fc07bb2d40edb20a8efeb02753df83c49aa63
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-03-02 11:10:52 +0900

    check vowel

---
+++ check_vowel.py
@@ -0,0 +1,19 @@
+"""
+ this program will take a character input and check if it a vowel or not.
+"""
+
+#a,e,i,o,u
+
+user=input('enter your charakter:')
+if user =='a':
+    print('the charakter is vowel')
+elif user =='e':
+    print('the charakter is vowel')
+elif user=='i':
+    print('the charakter is vowel')
+elif user =='o':
+    print('the charakter is vowel')
+elif user == 'u':
+    print('the charakter is vowel')
+else :
+    print('the charakter is a not vowel')                    
