ee14a6b2c0b1c247f220d4ef39e6e58f4c74ab89
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-03-02 10:56:19 +0900

    Voting system management

---
+++ voting system.py
@@ -0,0 +1,16 @@
+"""
+we need to devlop a program . it will tell us if a program is eligible for voting. it will take the person's age as input and tell Allowed to vote or Not Allowed to vote.
+but the country has a role, the females will be able to vote if they are 18 years and above and males will be able to vote if they are 20 years and above.
+"""
+"""
+1. enter input: from user age anf gender
+2. data verification: type validation( if input data are in correct formet or not)
+3. check eligibility: 
+    if gender == male and age>=20;
+        eligibile
+    elif gender == female and age>=18;
+        eligibile
+    else;
+        not eligibile    
+"""
+
